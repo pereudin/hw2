d4640d115234273ab71def3e2b0ccec412ad1a06
pereudin <a.a.pereudin@gmail.com>
2019-10-09 21:44:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/pereudin/hw2/AsyncTaskActivity.java
@@ -0,0 +1,14 @@
+package ru.pereudin.hw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AsyncTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
